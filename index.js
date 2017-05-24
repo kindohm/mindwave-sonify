2eba2c0fc9220aa8aafd87652bcc5f131b54d574
Mike Hodnick <mike@kindohm.com>
2017-05-24 12:37:06 -0500

    first

---
+++ index.js
@@ -0,0 +1,70 @@
+var Cylon = require('cylon');
+const express = require('express');
+const http = require('http');
+const url = require('url');
+const WebSocket = require('ws');
+
+const app = express();
+
+app.use('/static', express.static('public'))
+app.use('/node_modules', express.static('node_modules'))
+
+// app.use(function (req, res) {
+//   res.send({ msg: "hello" });
+// });
+
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+var mahWS;
+
+wss.on('connection', function connection(ws, req) {
+    mahWS = ws;
+    const location = url.parse(req.url, true);
+
+    ws.on('message', function incoming(message) {
+        console.log('received: %s', message);
+    });
+
+    ws.send('OH HAI.');
+});
+
+server.listen(8080, function listening() {
+    console.log('Listening on %d', server.address().port);
+});
+
+
+
+
+Cylon.robot({
+    connections: {
+        neurosky: { adaptor: 'neurosky', port: 'COM6' }
+    },
+
+    devices: {
+        headset: { driver: 'neurosky' }
+    },
+
+    work: function (my) {
+
+        console.log('work.');
+
+        my.headset.on('attention', function (data) {
+            console.log("attention:", data);
+            if (!mahWS) {
+                console.warn('no websocket');
+                return;
+            };
+            mahWS.send(JSON.stringify({type: 'attention', data: data}));
+        });
+
+        my.headset.on('meditation', function (data) {
+            console.log("meditation:", data);
+            if (!mahWS) {
+                console.warn('no websocket');
+                return;
+            };
+            mahWS.send(JSON.stringify({type: 'meditation', data: data}));
+        });
+    }
+}).start();
